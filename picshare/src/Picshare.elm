7bff53cadc7043615380ec12c8c38999c81b240c
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2019-04-04 22:39:33 +0900

    Add comment display

--- picshare/src/Picshare.elm
+++ picshare/src/Picshare.elm
@@ -2,20 +2,28 @@ module Picshare exposing (main)
 
 import Browser
 import Html exposing (..)
-import Html.Attributes exposing (class, src)
+import Html.Attributes exposing (class, placeholder, src, type_)
 import Html.Events exposing (onClick)
 
 baseUrl : String
 baseUrl =
     "https://programming-elm.com/"
 
-type alias Model = { url : String, caption : String, liked : Bool }
+type alias Model =
+    { url : String
+    , caption : String
+    , liked : Bool
+    , comments : List String
+    , newComment : String
+    }
 
 initialModel : Model
 initialModel =
     { url = baseUrl ++ "1.jpg"
     , caption = "Surfing"
     , liked = False
+    , comments = [ "Cowabunga, dude!" ]
+    , newComment = ""
     }
 
 viewLoveButton : Model -> Html Msg
@@ -43,6 +51,39 @@ viewDetailedPhoto model =
         , div [ class "photo-info" ]
             [ viewLoveButton model
             , h2 [ class "caption"] [ text model.caption ]
+            , viewComments model
+            ]
+        ]
+
+viewComment : String -> Html Msg
+viewComment comment =
+    li []
+        [ strong [] [ text "Comment:" ]
+        , text (" " ++ comment)
+        ]
+
+viewCommentList : List String -> Html Msg
+viewCommentList comments =
+    case comments of
+        [] ->
+            text ""
+        _ ->
+            div [ class "comments" ]
+                [ ul []
+                    (List.map viewComment comments)
+                ]
+
+viewComments : Model -> Html Msg
+viewComments model =
+    div []
+        [ viewCommentList model.comments
+        , form [ class "new-comment" ]
+            [ input
+                [ type_ "text"
+                , placeholder "Add a comment..."
+                ]
+                []
+            , button [] [ text "Save" ]
             ]
         ]
 
