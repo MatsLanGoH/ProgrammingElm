3d9cd8051e9e91e40b6d7e8fffa0116e7b5eb1db
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2019-04-06 22:24:56 +0900

    Reenable like/comment functionality

--- picshare/src/Picshare.elm
+++ picshare/src/Picshare.elm
@@ -42,9 +42,9 @@ photoDecoder =
         |> hardcoded ""
 
 type Msg
-    = ToggleLike
-    | UpdateComment String
-    | SaveComment
+    = ToggleLike Id
+    | UpdateComment Id String
+    | SaveComment Id
     | LoadFeed (Result Http.Error Feed)
 
 initialModel : Model
@@ -76,7 +76,7 @@ viewLoveButton photo =
         [ i
             [ class "fa fa-2x"
             , class buttonClass
-            -- , onClick ToggleLike
+            , onClick (ToggleLike photo.id)
             ]
             []
         ]
@@ -114,12 +114,12 @@ viewComments : Photo -> Html Msg
 viewComments photo =
     div []
         [ viewCommentList photo.comments
-        , form [ class "new-comment" {- , onSubmit SaveComment -} ]
+        , form [ class "new-comment" , onSubmit (SaveComment photo.id) ]
             [ input
                 [ type_ "text"
                 , placeholder "Add a comment..."
                 , value photo.newComment
-                , onInput UpdateComment
+                , onInput (UpdateComment photo.id)
                 ]
                 []
             , button
@@ -171,40 +171,50 @@ updateComment : String -> Photo -> Photo
 updateComment comment photo =
     { photo | newComment = comment }
 
-updateFeed : (Photo -> Photo) -> Maybe Photo -> Maybe Photo
-updateFeed updatePhoto maybePhoto =
-    Maybe.map updatePhoto maybePhoto
+updatePhotoById : (Photo -> Photo) -> Id -> Feed -> Feed
+updatePhotoById updatePhoto id feed =
+    List.map
+        (\photo ->
+            if photo.id == id then
+                updatePhoto photo
+
+            else
+                photo
+        )
+        feed
+
+updateFeed : (Photo -> Photo) -> Id -> Maybe Feed -> Maybe Feed
+updateFeed updatePhoto id maybeFeed =
+    Maybe.map (updatePhotoById updatePhoto id) maybeFeed
 
 update :
     Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        -- ToggleLike ->
-        --     ( { model
-        --         | photo = updateFeed toggleLike model.photo
-        --       }
-        --     , Cmd.none
-        --     )
-        -- UpdateComment comment ->
-        --     ( { model
-        --         | photo = updateFeed (updateComment comment) model.photo }
-        --     , Cmd.none
-        --     )
-        -- SaveComment ->
-        --     ( { model
-        --         | photo = updateFeed saveNewComment model.photo
-        --       }
-        --     , Cmd.none
-        --     )
+        ToggleLike id ->
+            ( { model
+                | feed = updateFeed toggleLike id model.feed
+              }
+            , Cmd.none
+            )
+
+        UpdateComment id comment ->
+            ( { model
+                | feed = updateFeed (updateComment comment) id model.feed }
+            , Cmd.none
+            )
+        SaveComment id ->
+            ( { model
+                | feed = updateFeed saveNewComment id model.feed
+              }
+            , Cmd.none
+            )
         LoadFeed (Ok feed) ->
             ( { model | feed = Just feed }
             , Cmd.none
             )
 
         LoadFeed (Err _) ->
-            ( model, Cmd.none )
-
-        _ ->
             ( model, Cmd.none )
 
 subscriptions : Model -> Sub Msg
