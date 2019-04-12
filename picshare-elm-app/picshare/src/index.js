13d73a112b15c36df86d30ad69c5b4ccd7d65bcc
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2019-04-13 00:53:47 +0900

    Transplant Picshare app onto create-elm-app framework

--- picshare-elm-app/picshare/src/index.js
+++ picshare-elm-app/picshare/src/index.js
@@ -1,9 +1,19 @@
 import './main.css';
-import { Elm } from './Main.elm';
+import { Elm } from './Picshare.elm';
 import registerServiceWorker from './registerServiceWorker';
 
-Elm.Main.init({
+var app = Elm.Picshare.init({
   node: document.getElementById('root')
 });
+
+app.ports.listen.subscribe(listen);
+
+function listen(url) {
+  var socket = new WebSocket(url);
+
+  socket.onmessage = function(event) {
+    app.ports.receive.send(event.data);
+  };
+}
 
 registerServiceWorker();
